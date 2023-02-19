92a59c5f929e2afa3aa898cabb988f581a60ea59
roxorneu <116113127+roxorneu@users.noreply.github.com>
2023-02-19 17:22:41 +0530

    Moved Drawer Nav to separate component

--- App.js
+++ App.js
@@ -4,93 +4,20 @@ import { LogBox, StatusBar } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { useFonts } from "expo-font";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ChoosePathScreen from "./app/screens/ChoosePathScreen";
-import CreateNewTripsScreen from "./app/screens/CreateNewTripScreen";
-import ViewTripsScreen from "./app/screens/ViewTripsScreen";
 import strings from "./app/config/strings";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
+
 import colors from "./app/config/colors";
-import ProfileScreen from "./app/screens/ProfileScreen";
 import fonts from "./app/config/fonts";
-import BackButton from "./app/components/BackButton";
-import ProfileButton from "./app/components/ProfileButton";
+
+import DrawerNavigator from "./app/navigation/DrawerNavigator";
 
 const Stack = createStackNavigator();
-
-// Choose Path Screen nests drawer into stack default stack navigator
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigation() {
-  return (
-    <Drawer.Navigator
-      initialRouteName={strings.choosePathScreen}
-      backBehavior="history"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.secondary,
-        },
-        headerTitleStyle: {
-          fontFamily: fonts.secondry,
-        },
-        headerTitleAlign: "center",
-        //headerStatusBarHeight: StatusBar.currentHeight,
-        swipeEnabled: true,
-        drawerPosition: "left",
-        drawerActiveTintColor: colors.primary,
-        drawerStyle: {
-          backgroundColor: colors.secondary,
-        },
-        drawerLabelStyle: {
-          fontFamily: fonts.secondry,
-        },
-      }}
-    >
-      <Drawer.Screen
-        name={strings.choosePathScreen}
-        component={ChoosePathScreen}
-        options={({ navigation }) => ({
-          headerTitleAlign: "center",
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          title: strings.choosePathScreen,
-          drawerItemStyle: {
-            display: "none",
-          },
-          headerRight: () => <ProfileButton navigation={navigation} />,
-        })}
-      />
-      <Drawer.Screen
-        name={strings.viewTripsScreen}
-        component={ViewTripsScreen}
-        options={({ navigation }) => ({
-          headerRight: () => <ProfileButton navigation={navigation} />,
-        })}
-      />
-      <Drawer.Screen
-        name={strings.createNewTripsScreen}
-        component={CreateNewTripsScreen}
-        options={({ navigation }) => ({
-          headerRight: () => <ProfileButton navigation={navigation} />,
-        })}
-      />
-
-      <Drawer.Screen
-        name={strings.profileScreen}
-        component={ProfileScreen}
-        options={({ navigation }) => ({
-          headerLeft: () => <BackButton navigation={navigation} />,
-        })}
-      />
-    </Drawer.Navigator>
-  );
-}
 
 const App = () => {
   LogBox.ignoreAllLogs();
@@ -147,7 +74,7 @@ const App = () => {
 
         <Stack.Screen
           name={strings.choosePathScreen}
-          component={DrawerNavigation}
+          component={DrawerNavigator}
           options={{
             headerStyle: {
               backgroundColor: colors.secondary,
---
+++ app/navigation/DrawerNavigator.js
@@ -0,0 +1,83 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import ChoosePathScreen from "../screens/ChoosePathScreen";
+import CreateNewTripsScreen from "../screens/CreateNewTripScreen";
+import ViewTripsScreen from "../screens/ViewTripsScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+
+import colors from "../config/colors";
+import fonts from "../config/fonts";
+
+import strings from "../config/strings";
+
+import BackButton from "../components/BackButton";
+import ProfileButton from "../components/ProfileButton";
+
+// Choose Path Screen nests drawer into stack default stack navigator
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigation() {
+  return (
+    <Drawer.Navigator
+      initialRouteName={strings.choosePathScreen}
+      backBehavior="history"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.secondary,
+        },
+        headerTitleStyle: {
+          fontFamily: fonts.secondry,
+        },
+        headerTitleAlign: "center",
+        //headerStatusBarHeight: StatusBar.currentHeight,
+        swipeEnabled: true,
+        drawerPosition: "left",
+        drawerActiveTintColor: colors.primary,
+        drawerStyle: {
+          backgroundColor: colors.secondary,
+        },
+        drawerLabelStyle: {
+          fontFamily: fonts.secondry,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name={strings.choosePathScreen}
+        component={ChoosePathScreen}
+        options={({ navigation }) => ({
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: colors.secondary,
+          },
+          title: strings.choosePathScreen,
+          drawerItemStyle: {
+            display: "none",
+          },
+          headerRight: () => <ProfileButton navigation={navigation} />,
+        })}
+      />
+      <Drawer.Screen
+        name={strings.viewTripsScreen}
+        component={ViewTripsScreen}
+        options={({ navigation }) => ({
+          headerRight: () => <ProfileButton navigation={navigation} />,
+        })}
+      />
+      <Drawer.Screen
+        name={strings.createNewTripsScreen}
+        component={CreateNewTripsScreen}
+        options={({ navigation }) => ({
+          headerRight: () => <ProfileButton navigation={navigation} />,
+        })}
+      />
+
+      <Drawer.Screen
+        name={strings.profileScreen}
+        component={ProfileScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => <BackButton navigation={navigation} />,
+        })}
+      />
+    </Drawer.Navigator>
+  );
+}
